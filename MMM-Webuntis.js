c5406cc821a93e70aa40d69e8c50972bdbfbc44d
thyed <thyed@users.noreply.github.com>
2020-01-22 22:30:19 +0100

    first version showing cancelled lessons for one student

---
+++ MMM-Webuntis.js
@@ -0,0 +1,118 @@
+Module.register("MMM-Webuntis", {
+
+    defaults: {
+        title: "",
+        school: "",
+        username: "",
+        password: "",
+        server: ""
+    },
+
+
+    getStyles: function () {
+  		return ["MMM-Webuntis.css"];
+  	},
+
+
+/*
+    getTranslations: function () {
+  		return {
+  			en: "translations/en.json",
+  			de: "translations/de.json"
+  		};
+  	},
+*/
+    start: function (){
+        let lessons = [];
+        this.sendSocketNotification("FETCH_DATA", this.config)
+    },
+
+    getDom: function() {
+        var wrapper = document.createElement("div");
+
+        var table = document.createElement("table");
+    		table.className = "normal medium regular";
+
+        if (this.lessons === undefined) {
+    			return table;
+    		}
+
+        // sort lessons by start time
+        this.lessons.sort((a,b) => a.dateString - b.dateString);
+
+        // iterate through lessons
+        for (let i = 0; i < this.lessons.length; i++) {
+            var lesson = this.lessons[i];
+            var time = new Date(lesson.year,lesson.month-1,lesson.day,lesson.hour,lesson.minutes);
+
+            // skip if nothing special
+            //if (lesson.code == '') continue;
+
+            // skip past lessons
+            if (time < new Date()) continue;
+
+            var row = document.createElement("tr");
+            table.appendChild(row);
+
+            // title, i.e. class name or child name
+            var titleCell = document.createElement("td");
+            titleCell.innerHTML = this.config.title;
+            titleCell.className = "align-right alignTop";
+            row.appendChild(titleCell);
+
+            // date and time
+            var dateTimeCell = document.createElement("td");
+            dateTimeCell.innerHTML = time.toLocaleDateString('de-DE',{weekday:'short'})
+              + "&nbsp;" + time.toLocaleTimeString('de-DE', {hour:'2-digit',minute:'2-digit'});
+            dateTimeCell.className = "leftSpace align-right alignTop";
+            row.appendChild(dateTimeCell);
+
+            // subject cell
+            var subjectCell = document.createElement("td");
+            subjectCell.innerHTML = this.capitalize(lesson.subject) + "&nbsp;("
+              + this.capitalize(lesson.teacher) + ")&nbsp;" + lesson.code;
+            if (lesson.text.length > 0 ) subjectCell.innerHTML += "</br><span class='small dimmed'>" + lesson.text + "</span>";
+            subjectCell.className = "leftSpace alignTop";
+            row.appendChild(subjectCell);
+
+        }
+
+        wrapper.appendChild(table);
+
+        return wrapper
+    },
+
+
+    capitalize: function(str) {
+      str = str.toLowerCase().split(" ");
+
+      for (let i = 0, x = str.length; i < x; i++) {
+        str[i] = str[i][0].toUpperCase() + str[i].substr(1);
+      }
+
+      return str.join(" ");
+    },
+
+    notificationReceived: function(notification, payload) {
+        switch(notification) {
+            case "DOM_OBJECTS_CREATED":
+                var timer = setInterval(() => {
+                    this.sendSocketNotification("FETCH_DATA", this.config)
+                }, 60*1000)
+            break;
+        }
+    },
+
+    socketNotificationReceived: function(notification, payload) {
+        switch(notification) {
+            case "GOT_DATA":
+                this.lessons = payload;
+                console.log(this.lessons);
+                this.updateDom();
+                break;
+        }
+    },
+
+
+
+  })
---
+++ node_helper.js
@@ -0,0 +1,70 @@
+const NodeHelper = require("node_helper");
+const WebUntis = require('webuntis');
+
+module.exports = NodeHelper.create({
+    start: function() {
+    },
+
+    socketNotificationReceived: function(notification, payload) {
+
+      switch(notification) {
+
+        case "FETCH_DATA":
+
+          // set config to config sent by client
+          this.config = payload;
+
+          const untis = new WebUntis(
+            this.config.school,
+            this.config.username,
+            this.config.password,
+            this.config.server
+          );
+
+          // create object with necessary date to be sent to module
+          var lessons = [];
+
+          untis
+              .login()
+              .then(session => {
+                var rangeStart = new Date();
+                var rangeEnd = new Date();
+                rangeEnd.setDate(rangeStart.getDate()+1);
+                return untis.getOwnTimetableForRange(rangeStart, rangeEnd);
+              })
+              .then(timetable => {
+
+                timetable.forEach(element => {
+                  let lesson = {};
+  								let year = element.date.toString().substring(0,4);
+  								let month = element.date.toString().substring(4,6);
+  								let day = element.date.toString().substring(6);
+  								let hour = element.startTime.toString();
+  								hour.length == 3 ? hour = '0'+hour.substring(0,1) : hour = hour.substring(0,2);
+  								let minutes = element.startTime.toString()
+  								minutes = minutes.substring(minutes.length-2);
+  								lesson.dateString = element.date + hour + minutes;
+                  lesson.year = year;
+                  lesson.month = month;
+                  lesson.day = day;
+                  lesson.hour = hour;
+                  lesson.minutes = minutes;
+  								lesson.subject = element.su[0].longname;
+  								lesson.teacher = element.te[0].longname;
+  								element.code ? lesson.code = element.code : lesson.code = '';
+  								element.lstext ? lesson.text = element.lstext : lesson.text = '';
+
+  								lessons.push(lesson);
+                });
+
+                this.sendSocketNotification("GOT_DATA", lessons);
+              })
+              .catch(error => {
+                console.log("ERROR: Could not get valid sesssion. " + error);
+              });
+
+          untis.logout();
+          break;
+      }
+    },
+  })
