ac921e1e6437b4e89e4d14465271bb6bd1eff03a
thyed <thyed@users.noreply.github.com>
2020-08-16 23:19:58 +0200

    hotfix for returning error object

--- MMM-Webuntis.js
+++ MMM-Webuntis.js
@@ -1,151 +1,148 @@
 Module.register("MMM-Webuntis", {
 
-    defaults: {
-        students: [
-          {
-            title: "SET CONFIG!",
-            school: "",
-            username: "",
-            password: "",
-            server: ""
-          },
-        ],
-        days: 7,
-        fetchInterval: 5 * 60 * 1000,
-        showStartTime: false
-      },
+	defaults: {
+		students: [
+			{
+				title: "SET CONFIG!",
+				school: "",
+				username: "",
+				password: "",
+				server: ""
+			},
+		],
+		days: 7,
+		fetchInterval: 5 * 60 * 1000,
+		showStartTime: false
+	},
 
-
-    getStyles: function () {
+	getStyles: function () {
   		return ["MMM-Webuntis.css"];
   	},
 
-    getTranslations: function () {
+	getTranslations: function () {
   		return {
   			en: "translations/en.json",
   			de: "translations/de.json"
   		};
   	},
 
-    start: function (){
-        this.lessonsByStudent = [];
-        this.sendSocketNotification("FETCH_DATA", this.config)
-    },
+	start: function (){
+		this.lessonsByStudent = [];
+		this.sendSocketNotification("FETCH_DATA", this.config);
+	},
 
-    getDom: function() {
-        var wrapper = document.createElement("div");
+	getDom: function() {
+		var wrapper = document.createElement("div");
 
-        var table = document.createElement("table");
+		var table = document.createElement("table");
     		table.className = "bright small light";
 
-        // no student
-        if (this.lessonsByStudent === undefined) {
+		// no student
+		if (this.lessonsByStudent === undefined) {
     			return table;
     		}
 
-        var addedRows = 0;
+		var addedRows = 0;
 
-        // iterate through students
-        // TODO: for..in does not guarantee specific order
-        for (let studentTitle in this.lessonsByStudent) {
-        //for (const [studentTitle, lessons] of this.lessonsByStudent.entries()) {
+		// iterate through students
+		// TODO: for..in does not guarantee specific order
+		for (let studentTitle in this.lessonsByStudent) {
+			//for (const [studentTitle, lessons] of this.lessonsByStudent.entries()) {
 
-          var lessons = this.lessonsByStudent[studentTitle];
+			var lessons = this.lessonsByStudent[studentTitle];
 
-          // sort lessons by start time
-          lessons.sort((a,b) => a.sortString - b.sortString);
+			// sort lessons by start time
+			lessons.sort((a,b) => a.sortString - b.sortString);
 
-          // iterate through lessons of current student
-          for (let i = 0; i < lessons.length; i++) {
-              var lesson = lessons[i];
-              var time = new Date(lesson.year,lesson.month-1,lesson.day,lesson.hour,lesson.minutes);
+			// iterate through lessons of current student
+			for (let i = 0; i < lessons.length; i++) {
+				var lesson = lessons[i];
+				var time = new Date(lesson.year,lesson.month-1,lesson.day,lesson.hour,lesson.minutes);
 
-              // skip if nothing special
-              if (lesson.code == '') continue;
+				// skip if nothing special
+				if (lesson.code == "") {continue;}
 
-              // skip past lessons
-              if (time < new Date() && lesson.code != 'error') continue;
+				// skip past lessons
+				if (time < new Date() && lesson.code != "error") {continue;}
 
-              addedRows++;
+				addedRows++;
 
-              var row = document.createElement("tr");
-              table.appendChild(row);
+				var row = document.createElement("tr");
+				table.appendChild(row);
 
-              // title, i.e. class name or child name
-              var titleCell = document.createElement("td");
-              titleCell.innerHTML = studentTitle;
-              titleCell.className = "align-right alignTop";
-              row.appendChild(titleCell);
+				// title, i.e. class name or child name
+				var titleCell = document.createElement("td");
+				titleCell.innerHTML = studentTitle;
+				titleCell.className = "align-right alignTop";
+				row.appendChild(titleCell);
 
-              // date and time
-              var dateTimeCell = document.createElement("td");
-              dateTimeCell.innerHTML = time.toLocaleDateString('de-DE',{weekday:'short'}).toUpperCase() + "&nbsp;"
-              if (this.config.showStartTime || lesson.lessonNumber === undefined) dateTimeCell.innerHTML += time.toLocaleTimeString('de-DE', {hour:'2-digit',minute:'2-digit'});
-              else dateTimeCell.innerHTML += lesson.lessonNumber + ".";
-              dateTimeCell.className = "leftSpace align-right alignTop";
-              row.appendChild(dateTimeCell);
+				// date and time
+				var dateTimeCell = document.createElement("td");
+				dateTimeCell.innerHTML = time.toLocaleDateString("de-DE",{weekday:"short"}).toUpperCase() + "&nbsp;";
+				if (this.config.showStartTime || lesson.lessonNumber === undefined) {dateTimeCell.innerHTML += time.toLocaleTimeString("de-DE", {hour:"2-digit",minute:"2-digit"});}
+				else {dateTimeCell.innerHTML += lesson.lessonNumber + ".";}
+				dateTimeCell.className = "leftSpace align-right alignTop";
+				row.appendChild(dateTimeCell);
 
-              // subject cell
-              var subjectCell = document.createElement("td");
-              subjectCell.innerHTML = lesson.substText;
-              if (lesson.substText == '') subjectCell.innerHTML =
+				// subject cell
+				var subjectCell = document.createElement("td");
+				subjectCell.innerHTML = lesson.substText;
+				if (lesson.substText == "") {subjectCell.innerHTML =
                 this.capitalize(lesson.subject) + "&nbsp;(" +
-                this.capitalize(lesson.teacher) + ")";
-              //if (lesson.text.length > 0 ) subjectCell.innerHTML += "</br><span class='xsmall dimmed'>" + lesson.text + "</span>";
-              subjectCell.className = "leftSpace align-left alignTop";
-              if (lesson.code == 'cancelled') subjectCell.className += " cancelled";
-              if (lesson.code == 'error') subjectCell.className += " error";
+                this.capitalize(lesson.teacher) + ")";}
+				//if (lesson.text.length > 0 ) subjectCell.innerHTML += "</br><span class='xsmall dimmed'>" + lesson.text + "</span>";
+				subjectCell.className = "leftSpace align-left alignTop";
+				if (lesson.code == "cancelled") {subjectCell.className += " cancelled";}
+				if (lesson.code == "error") {subjectCell.className += " error";}
 
-              row.appendChild(subjectCell);
-          } // end for lessons
-        } // end for students
+				row.appendChild(subjectCell);
+			} // end for lessons
+		} // end for students
 
-        // add message row if table is empty
-        if (addedRows == 0) {
-          var nothingRow = document.createElement("tr");
-          table.appendChild(nothingRow);
-          var nothingCell = document.createElement("td");
-          nothingCell.innerHTML = this.translate("nothing");
-          nothingRow.appendChild(nothingCell);
-        }
+		// add message row if table is empty
+		if (addedRows == 0) {
+			var nothingRow = document.createElement("tr");
+			table.appendChild(nothingRow);
+			var nothingCell = document.createElement("td");
+			nothingCell.innerHTML = this.translate("nothing");
+			nothingRow.appendChild(nothingCell);
+		}
 
-        wrapper.appendChild(table);
+		wrapper.appendChild(table);
 
-        return wrapper
-    },
+		return wrapper;
+	},
 
-    capitalize: function(str) {
-      str = str.toLowerCase().split(" ");
+	capitalize: function(str) {
+		str = str.toLowerCase().split(" ");
 
-      for (let i = 0, x = str.length; i < x; i++) {
-        if (str[i]) {
-          if (str[i] === "ii" || str[i] === "iii") str[i] = str[i].toUpperCase()
-          else str[i] = str[i][0].toUpperCase() + str[i].substr(1);
-        }
-      }
+		for (let i = 0, x = str.length; i < x; i++) {
+			if (str[i]) {
+				if (str[i] === "ii" || str[i] === "iii") {str[i] = str[i].toUpperCase();}
+				else {str[i] = str[i][0].toUpperCase() + str[i].substr(1);}
+			}
+		}
 
-      return str.join(" ");
-    },
+		return str.join(" ");
+	},
 
-    notificationReceived: function(notification, payload) {
-        switch(notification) {
-            case "DOM_OBJECTS_CREATED":
-                var timer = setInterval(() => {
-                    this.sendSocketNotification("FETCH_DATA", this.config)
-                }, this.config.fetchInterval)
-            break;
-        }
-    },
+	notificationReceived: function(notification, payload) {
+		switch(notification) {
+		case "DOM_OBJECTS_CREATED":
+			var timer = setInterval(() => {
+				this.sendSocketNotification("FETCH_DATA", this.config);
+			}, this.config.fetchInterval);
+			break;
+		}
+	},
 
-    socketNotificationReceived: function(notification, payload) {
-        if (notification === "GOT_DATA") {
-          if (payload.lessons) {
-            this.lessonsByStudent[payload.title] = payload.lessons;
-            this.updateDom();
-          }
-        }
-    },
+	socketNotificationReceived: function(notification, payload) {
+		if (notification === "GOT_DATA") {
+			if (payload.lessons) {
+				this.lessonsByStudent[payload.title] = payload.lessons;
+				this.updateDom();
 
-
-
-  })
+			}
+		}
+	},
+});
--- node_helper.js
+++ node_helper.js
@@ -1,109 +1,111 @@
 const NodeHelper = require("node_helper");
-const WebUntis = require('webuntis');
+const WebUntis = require("webuntis");
 
 module.exports = NodeHelper.create({
-    start: function() {
-    },
+	start: function() {
+	},
 
-    socketNotificationReceived: function(notification, payload) {
+	socketNotificationReceived: function(notification, payload) {
 
-      if (notification === "FETCH_DATA") {
+		if (notification === "FETCH_DATA") {
 
-        // iterate through students, fetch and send lessons
-        for (let i in payload.students) {
-          this.fetchLessons(payload.students[i], payload.days);
-        }
-      }
-    },
+			// iterate through students, fetch and send lessons
+			for (let i in payload.students) {
+				this.fetchLessons(payload.students[i], payload.days);
+			}
+		}
+	},
 
+	fetchLessons: function(studentData, days) {
 
-    fetchLessons: function(studentData, days) {
+		const untis = new WebUntis(
+			studentData.school,
+			studentData.username,
+			studentData.password,
+			studentData.server
+		);
 
-        const untis = new WebUntis(
-          studentData.school,
-          studentData.username,
-          studentData.password,
-          studentData.server
-        );
+		if (days<1 || days>10 || isNaN(days)) {days = 1;}
 
-        if (days<1 || days>10 || isNaN(days)) days = 1;
+		// create lessons array to be sent to module
+		var lessons = [];
 
-        // create lessons array to be sent to module
-        var lessons = [];
+		// array to get lesson number by start time
+		var startTimes = [];
 
-        // array to get lesson number by start time
-        var startTimes = [];
+		untis
+			.login()
+			.then(response => {
+				var rangeStart = new Date();
+				var rangeEnd = new Date();
+				rangeEnd.setDate(rangeStart.getDate()+days);
 
-        untis
-            .login()
-            .then(response => {
-              var rangeStart = new Date();
-              var rangeEnd = new Date();
-              rangeEnd.setDate(rangeStart.getDate()+days);
+				untis.getTimegrid()
+					.then(grid => {
+						// use grid of first day and assume all days are the same
+						grid[0].timeUnits.forEach(element => {
+							startTimes[element.startTime] = element.name;
+						})
 
-              untis.getTimegrid()
-              .then(grid => {
-                // use grid of first day and assume all days are the same
-                grid[0].timeUnits.forEach(element => {
-                  startTimes[element.startTime] = element.name;
-                })
+					})
+					.catch(error => {
+						console.log("Error in getTimegrid: " + error);
+					})
 
-              })
-              .catch(error => {
-                console.log("Error in getTimegrid: " + error);
-              })
+				return untis.getOwnTimetableForRange(rangeStart, rangeEnd);
+			})
+			.then(timetable => {
 
-              return untis.getOwnTimetableForRange(rangeStart, rangeEnd);
-            })
-            .then(timetable => {
+				timetable.forEach(element => {
+					let lesson = {};
+					let year = element.date.toString().substring(0,4);
+					let month = element.date.toString().substring(4,6);
+					let day = element.date.toString().substring(6);
+					let hour = element.startTime.toString();
+					hour.length == 3 ? hour = "0"+hour.substring(0,1) : hour = hour.substring(0,2);
+					let minutes = element.startTime.toString()
+					minutes = minutes.substring(minutes.length-2);
+					lesson.sortString = element.date + hour + minutes;
+					switch (element.code) {
+					case "cancelled": lesson.sortString += "1"; break;
+					case "irregular": lesson.sortString += "2"; break;
+					default: lesson.sortString += "9";
+					}
+					lesson.year = year;
+					lesson.month = month;
+					lesson.day = day;
+					lesson.hour = hour;
+					lesson.minutes = minutes;
+					lesson.lessonNumber = startTimes[element.startTime];
+					element.su[0] ? lesson.subject = element.su[0].longname : lesson.subject = "";
+					element.te[0] ? lesson.teacher = element.te[0].longname : lesson.teacher = "";
+					element.code ? lesson.code = element.code : lesson.code = "";
+					element.lstext ? lesson.text = element.lstext : lesson.text = "";
+					element.substText ? lesson.substText = element.substText : lesson.substText = "";
 
-              timetable.forEach(element => {
-                let lesson = {};
-								let year = element.date.toString().substring(0,4);
-								let month = element.date.toString().substring(4,6);
-								let day = element.date.toString().substring(6);
-								let hour = element.startTime.toString();
-								hour.length == 3 ? hour = '0'+hour.substring(0,1) : hour = hour.substring(0,2);
-								let minutes = element.startTime.toString()
-								minutes = minutes.substring(minutes.length-2);
-								lesson.sortString = element.date + hour + minutes;
-                switch (element.code) {
-                  case "cancelled": lesson.sortString += "1"; break;
-                  case "irregular": lesson.sortString += "2"; break;
-                  default: lesson.sortString += "9";
-                }
-                lesson.year = year;
-                lesson.month = month;
-                lesson.day = day;
-                lesson.hour = hour;
-                lesson.minutes = minutes;
-                lesson.lessonNumber = startTimes[element.startTime];
-								element.su[0] ? lesson.subject = element.su[0].longname : lesson.subject = '';
-								element.te[0] ? lesson.teacher = element.te[0].longname : lesson.teacher = '';
-								element.code ? lesson.code = element.code : lesson.code = '';
-								element.lstext ? lesson.text = element.lstext : lesson.text = '';
-                element.substText ? lesson.substText = element.substText : lesson.substText = '';
+					lessons.push(lesson);
+				});
 
-								lessons.push(lesson);
-              });
+				this.sendSocketNotification("GOT_DATA", {title: studentData.title, lessons: lessons});
+			})
+			.catch(error => {
+				console.log("ERROR for " + studentData.title + ": " + error.toString());
+				/*
+				let today = new Date();
+				let errorObject = [ {
+					year: today.getFullYear(),
+					month: today.getMonth()+1,
+					day: today.getDate(),
+					hour: today.getHours(),
+					minutes: today.getMinutes(),
+					subject: "ERROR",
+					teacher: error.toString(),
+					code: "error"
+				} ];
+        		this.sendSocketNotification("GOT_DATA", {title: studentData.title, lessons: errorObject});
+        		*/
+			});
 
-              this.sendSocketNotification("GOT_DATA", {title: studentData.title, lessons: lessons});
-            })
-            .catch(error => {
-              let today = new Date();
-              let errorObject = [ {
-                year: today.getFullYear(),
-                month: today.getMonth()+1,
-                day: today.getDate(),
-                hour: today.getHours(),
-                minutes: today.getMinutes(),
-                subject: "ERROR",
-                teacher: error.toString(),
-                code: "error"
-              } ];
-              this.sendSocketNotification("GOT_DATA", {title: studentData.title, lessons: errorObject});
-            });
-
-        untis.logout();
-      },
-  })
+		untis.logout();
+	},
+})
