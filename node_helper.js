ba09ec5d810ed2c7a1b8888ee803165ef09ad83c
ar-wed <arwed.me@me.com>
2022-04-24 12:12:33 +0200

    Cleanup of timetableToLessons function

--- node_helper.js
+++ node_helper.js
@@ -169,30 +169,38 @@ module.exports = NodeHelper.create({
 		var lessons = [];
 		timetable.forEach(element => {
 			let lesson = {};
-			let year = element.date.toString().substring(0,4);
-			let month = element.date.toString().substring(4,6);
-			let day = element.date.toString().substring(6);
-			let hour = element.startTime.toString();
-			hour.length == 3 ? hour = "0"+hour.substring(0,1) : hour = hour.substring(0,2);
-			let minutes = element.startTime.toString()
-			minutes = minutes.substring(minutes.length-2);
-			lesson.sortString = element.date + hour + minutes;
-			switch (element.code) {
-			case "cancelled": lesson.sortString += "1"; break;
-			case "irregular": lesson.sortString += "2"; break;
-			default: lesson.sortString += "9";
-			}
-			lesson.year = year;
-			lesson.month = month;
-			lesson.day = day;
-			lesson.hour = hour;
-			lesson.minutes = minutes;
+
+			//Parse date and time information
+			lesson.year = element.date.toString().substring(0,4);
+			lesson.month = element.date.toString().substring(4,6);
+			lesson.day = element.date.toString().substring(6);
+			lesson.hour = element.startTime.toString();
+			lesson.hour = (lesson.hour.length == 3 ? ("0" + lesson.hour.substring(0,1)) : lesson.hour.substring(0,2));
+			lesson.minutes = element.startTime.toString();
+			lesson.minutes = lesson.minutes.substring(lesson.minutes.length-2);
+			
+			//Parse lesson number by start time
 			lesson.lessonNumber = startTimes[element.startTime];
-			element.su[0] ? lesson.subject = element.su[0].longname : lesson.subject = "";
-			element.te ? lesson.teacher = element.te[0].longname : lesson.teacher = "";
-			element.code ? lesson.code = element.code : lesson.code = "";
-			element.lstext ? lesson.text = element.lstext : lesson.text = "";
-			element.substText ? lesson.substText = element.substText : lesson.substText = "";
+
+			//Parse data about teacher
+			lesson.teacher = element.te[0] ? element.te[0].longname : "";
+
+			//Parse data about subject
+			lesson.subject = element.su[0] ? element.su[0].longname : "";
+
+			//Parse other information
+			lesson.code = element.code ? element.code : "";
+			lesson.text = element.lstext ? element.lstext : "";
+			lesson.substText = element.substText ? element.substText : "";
+			
+			//Create sort string
+			lesson.sortString = lesson.year + lesson.month + lesson.day + lesson.hour + lesson.minutes;
+			switch (lesson.code) {
+				case "cancelled": lesson.sortString += "1"; break;
+				case "irregular": lesson.sortString += "2"; break;
+				default: lesson.sortString += "9";
+			}
+
 
 			lessons.push(lesson);
 		});
